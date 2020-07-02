ae72dfb73ea5f75537e53803b03028cc60215423
Maxym Martynenkow <martymaxon@gmail.com>
2020-07-02 12:54:47 +0300

    first commit

---
+++ Septik.Web/Models/CityDetailsVM.cs
@@ -0,0 +1,18 @@
+﻿using Septik.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Septik.Web.Models
+{
+    public class CityDetailsVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Lat { get; set; }
+        public string Lng { get; set; }
+        public List<CityImage> CityImages { get; set; }
+    }
+}
